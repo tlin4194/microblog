27659f718e3b3660d8a3c9aa48cdf2cbb0f3048a
Tiffany <chiangt1tiff@gmail.com>
2024-10-04 20:20:27 -0400

    modify v0.2 to explain templates

--- app/routes.py
+++ app/routes.py
@@ -5,15 +5,49 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Alice'}
+    return f'''
+    <html>
+        <head>
+            <title>Home Page - Microblog</title>
+        </head>
+        <body>
+            <h1>Hello, {user["username"]}! This is a level 1 Header.</h1>
+            <h2>This is a level 2 Header.</h2>
+            <div><p>This is a paragraph! <b>Some bold text</b></p></div>
+        </body>
+    </html>'''
+
+
+@app.route('/index_v2')
+def index_v2():
+    user1 = {'username': 'Alice'}
+    user2 = {'username': 'Bob'}
     posts = [
         {
-            'author': {'username': 'John'},
+            'author': user1,
             'body': 'Beautiful day in Portland!'
         },
         {
-            'author': {'username': 'Susan'},
+            'author': user2,
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title=user1["username"], user=user1, posts=posts)
+
+
+@app.route('/index_v3')
+def index_v3():
+    user1 = {'username': 'Alice'}
+    user2 = {'username': 'Bob'}
+    posts = [
+        {
+            'author': user1,
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': user2,
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index2.html', title=user1["username"], user=user1, posts=posts)
